660d5319cd53bf30da1e0f24d9d3ad494ffa3672
adriansmartup <123541937+adriansmartup@users.noreply.github.com>
2023-04-11 13:14:34 +0200

    Create configuration.py

---
+++ app/configuration.py
@@ -0,0 +1,19 @@
+# -*- Coding: UTF-8 -*-   #
+# @created: 21/11/2017    #
+# @author: Julio Lafuente #
+# @author: DxD            #
+# @version: 1.0           #
+#        Enviroment       #
+#        DEV / PRO        #
+ENVIRONMENT = "PRO"
+#        LOG LEVEL        #
+#        ERROR = 40       #
+#        WARNING = 30     #
+#        INFO = 20        #
+#        DEBUG = 10       #
+LOG_LEVEL = 20
+#       Elasticsearch     #
+ES_HOST = 'http://biv-aodback-01.aragon.local:9200'
+#       Accounts Keys     #
+GA_KEY = 'aragon-open-data-cdc486240cc3.json'
+URCHIN_KEY = "ChangeMe"

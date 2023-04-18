274d3b22c7df0fab113043dda96df198a8bd9d9b
adriansmartup <123541937+adriansmartup@users.noreply.github.com>
2023-04-18 12:11:08 +0200

    Se añade la configuración para el servidor de desarrollo

--- app/configuration.py
+++ app/configuration.py
@@ -1,11 +1,11 @@
 # -*- Coding: UTF-8 -*-   #
-# @created: 21/11/2017    #
+# @created: 16/04/2023    #
 # @author: Julio Lafuente #
 # @author: DxD            #
 # @version: 1.0           #
 #        Enviroment       #
 #        DEV / PRO        #
-ENVIRONMENT = "PRO"
+ENVIRONMENT = "DES"
 #        LOG LEVEL        #
 #        ERROR = 40       #
 #        WARNING = 30     #
@@ -13,7 +13,8 @@ ENVIRONMENT = "PRO"
 #        DEBUG = 10       #
 LOG_LEVEL = 20
 #       Elasticsearch     #
-ES_HOST = 'http://biv-aodback-01.aragon.local:9200'
+ES_HOST = 'http://user:password@miv-aodei2a-01.aragon.local:9200'
+#user:password from elastic
 #       Accounts Keys     #
 GA_KEY = 'aragon-open-data-cdc486240cc3.json'
 URCHIN_KEY = "ChangeMe"
